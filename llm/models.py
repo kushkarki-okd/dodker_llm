cacba27bf43b1285cb3ed0723ff5204ac96a028c
kushkarki <alwaysdoyourbest123@gmail.com>
2026-04-05 16:26:43 +0545

    first llm commit

---
+++ llm/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class chat(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+class message(models.Model):
+    chat=models.ForeignKey(chat, on_delete=models.CASCADE)
+    sender=models.CharField(max_length=15)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
